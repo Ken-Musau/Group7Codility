cf373b344fd7fd72eb852396153883f76c8d39f5
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-11 19:29:06 +0300

    refactore qn 2

--- qn2.rb
+++ qn2.rb
@@ -1,12 +1,12 @@
 def longest_consecutive(nums)
   nums = nums.uniq.sort
   longest_streak = 0
-  current_streak = 1
+  current_streak = 0
 
-  (1...nums.length).each do |i|
-    if nums[i] == nums[i - 1] + 1
+  nums.each_with_index do |num, i|
+    if i > 0 && num == nums[i - 1] + 1
       current_streak += 1
-    elsif nums[i] != nums[i - 1]
+    elsif i == 0 || num != nums[i - 1] + 1
       longest_streak = [longest_streak, current_streak].max
       current_streak = 1
     end
