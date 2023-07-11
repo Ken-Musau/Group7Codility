65cfa84091fb4e9b1ea0a828863cb279455b31a7
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-11 19:30:57 +0300

    refactor qn 1

--- qn1.rb
+++ qn1.rb
@@ -3,14 +3,7 @@
 # given an array A consisting of N integers, returns the maximum among all integers which are multiples of 4.For example, given array A as follows:[-6, -91, 1011, -100, 84, -22, 0, 1, 473]the function should return 84.
 
 def max_multiple_of_four(n)
-  multiples_of_4 = []
-
-  n.each do |num|
-    if num % 4 == 0
-      multiples_of_4 << num
-    end
-  end
-  multiples_of_4.max
+  n.select { |num| num % 4 == 0 }.max
 end
 
 arr = [-6, -91, 1011, -100, 84, -22, 0, 1, 473]
